09e2eee1d3d01e02d1ddf2fc1001be7d4aeab5d1
J-Bentley <JordanBentley77@gmail.com>
2023-12-18 22:06:44 -0500

    added jumpad sound

--- Assets/Scripts/Jumpad.cs
+++ Assets/Scripts/Jumpad.cs
@@ -4,10 +4,16 @@ using UnityEngine;
 public class Jumpad : MonoBehaviour {
     public Player playerScript;
     [SerializeField] private float jumpadHeight;
+    private AudioSource audioSource;
+
+    void Start() {
+        audioSource = GetComponent<AudioSource>();
+    }
 
     void OnTriggerEnter(Collider collider) {
         if (collider.transform.tag == "Player") {
             playerScript.Jumpad(jumpadHeight);
+            audioSource.Play();
         }
     }
 }
