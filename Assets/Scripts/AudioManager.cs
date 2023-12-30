5156810cb984ea0b2597cf578d08c9487127e645
J-Bentley <JordanBentley77@gmail.com>
2023-12-30 06:21:27 -0500

    l

--- Assets/Scripts/AudioManager.cs
+++ Assets/Scripts/AudioManager.cs
@@ -1,9 +1,20 @@
 using UnityEngine;
 using UnityEngine.Audio;
+using UnityEngine.UI;
 
 public class AudioScript : MonoBehaviour {
 
     public AudioMixer masterMixer;
+    [SerializeField] private Slider musicSlider;
+    [SerializeField] private Slider sfxSlider;
+
+    void Start() {
+        masterMixer.GetFloat("musicVol", out float musicVolume);
+        musicSlider.value = musicVolume;
+
+        masterMixer.GetFloat("sfxVol", out float sfxVolume);
+        sfxSlider.value = sfxVolume;
+    }
 
     public void SetMusicVolume(float soundLevel) {
         masterMixer.SetFloat ("musicVol", soundLevel);
