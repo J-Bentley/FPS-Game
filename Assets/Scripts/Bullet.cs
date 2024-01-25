8dbe5520aa4888e43a0537c0ce0b866a01741177
J-Bentley <JordanBentley77@gmail.com>
2024-01-25 01:27:42 -0500

    v

--- Assets/Scripts/Bullet.cs
+++ Assets/Scripts/Bullet.cs
@@ -4,33 +4,32 @@ public class Bullet : MonoBehaviour {
     [SerializeField] ParticleSystem impact;
     [SerializeField] ParticleSystem fleshImpact;
     [SerializeField] ParticleSystem bulletHole;
-    private ParticleSystem particlesystem;
-    [SerializeField] CharacterController playerObject;
-    private int collisionCount;
+    [SerializeField] ParticleSystem fleshBulletHole;
+    private ParticleSystem impactParticlesystem;
+    private ParticleSystem bulletHoleParticlesystem;
 
     void OnCollisionEnter(Collision collision) {
-        collisionCount++;
-        if (collisionCount == 1) {
-            Target target = collision.gameObject.transform.GetComponent<Target>();
-            if (target != null) {
-                target.TakeTargetDamage(Gun.damage);
-                particlesystem = fleshImpact;
-            } else {
-                particlesystem = impact;
-            }
-            
-            Quaternion normalizedRot = Quaternion.FromToRotation(Vector3.up, collision.contacts[0].normal);
-            ParticleSystem impactInstance = Instantiate(particlesystem, collision.contacts[0].point, normalizedRot);
-            Destroy(impactInstance.gameObject, 3f);
-
-            ParticleSystem bulletHoleInstance = Instantiate(bulletHole, collision.contacts[0].point, normalizedRot);
-            bulletHoleInstance.transform.parent = collision.gameObject.transform;
-
-            if (collision.gameObject.transform.GetComponent<Rigidbody>() != null) {
-                collision.gameObject.transform.GetComponent<Rigidbody>().AddForce(transform.forward * Gun.bulletForce, ForceMode.Impulse);
-            }
+        Target target = collision.gameObject.transform.GetComponent<Target>();
+        if (target != null) {
+            target.TakeTargetDamage(Gun.damage);
+            impactParticlesystem = fleshImpact;
+            bulletHoleParticlesystem = fleshBulletHole;
         } else {
-            Destroy(gameObject, 3f); 
+            impactParticlesystem = impact;
+            bulletHoleParticlesystem = bulletHole;
         }
+            
+        Quaternion normalizedRot = Quaternion.FromToRotation(Vector3.up, collision.contacts[0].normal);
+
+        Instantiate(impactParticlesystem, collision.contacts[0].point, normalizedRot);
+
+        ParticleSystem bulletHoleInstance = Instantiate(bulletHoleParticlesystem, collision.contacts[0].point, normalizedRot);
+        bulletHoleInstance.transform.parent = collision.gameObject.transform;
+
+        if (collision.gameObject.transform.GetComponent<Rigidbody>() != null) {
+            collision.gameObject.transform.GetComponent<Rigidbody>().AddForce(transform.forward * Gun.bulletForce, ForceMode.Impulse);
+        }
+        
+        Destroy(gameObject); 
     }
 }
