f7f4952652cf449725e697a1c189e9a6ff00f407
J-Bentley <JordanBentley77@gmail.com>
2024-01-28 02:51:13 -0500

    h

--- Assets/Scripts/Recoil.cs
+++ Assets/Scripts/Recoil.cs
@@ -1,49 +1,34 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
-public class Recoil : MonoBehaviour
-{
-    Vector3 currentRotation, targetRotation, targetPosition, currentPosition, initialGunPosition;
-    public Transform cam;
+public class Recoil : MonoBehaviour {
+    public Gun gunScript;
+    [SerializeField] private Transform cam;
+    [SerializeField] private float recoilX, recoilY, recoilZ, kickBackZ, snappiness, returnAmount;
+    private Vector3 currentRotation, targetRotation, targetPosition, currentPosition, originalPositon;
 
-    public float recoilX;
-    public float recoilY;
-    public float recoilZ;
-
-    public float kickBackZ;
-
-    public float snappiness, returnAmount;
-    void Start()
-    {
-        initialGunPosition = transform.localPosition;
+    void Start() {
+        originalPositon = transform.localPosition;
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-       
-        targetRotation = Vector3.Lerp(targetRotation, Vector3.zero, Time.deltaTime * returnAmount);
-        currentRotation = Vector3.Slerp(currentRotation, targetRotation, Time.fixedDeltaTime * snappiness);
-        transform.localRotation = Quaternion.Euler(currentRotation);
-        cam.localRotation = Quaternion.Euler(currentRotation);
-        back();//kickback
-
+    void Update() {
+        if (gunScript.gunEquipped) {
+            targetRotation = Vector3.Lerp(targetRotation, Vector3.zero, Time.deltaTime * returnAmount);
+            currentRotation = Vector3.Slerp(currentRotation, targetRotation, Time.fixedDeltaTime * snappiness);
+            transform.localRotation = Quaternion.Euler(currentRotation);
+            cam.localRotation = Quaternion.Euler(currentRotation);
+            Down();
+        }
     }
-    public void recoil()
 
-    {
+    public void Up() {
         targetPosition -= new Vector3(0, 0, kickBackZ);
         targetRotation += new Vector3(recoilX, Random.Range(-recoilY, recoilY), Random.Range(-recoilZ, recoilZ));
-        back();
+        Down();
     }
-    void back()
-    {
-        targetPosition = Vector3.Lerp(targetPosition, initialGunPosition, Time.deltaTime * returnAmount);
+
+    void Down() {
+        targetPosition = Vector3.Lerp(targetPosition, originalPositon, Time.deltaTime * returnAmount);
         currentPosition = Vector3.Lerp(currentPosition, targetPosition, Time.fixedDeltaTime * snappiness);
         transform.localPosition = currentPosition;
     }
 }
-
-
-//see my procedural recoil video it have the camera shake when shooting
