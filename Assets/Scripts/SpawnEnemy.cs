d11bdfce34e46d56f5811e9165b4d2789a76a8db
J-Bentley <JordanBentley77@gmail.com>
2024-08-26 23:53:44 -0400

    x

--- Assets/Scripts/SpawnEnemy.cs
+++ Assets/Scripts/SpawnEnemy.cs
@@ -2,35 +2,23 @@ using UnityEngine;
 
 public class SpawnEnemy : MonoBehaviour {
 
-    GameObject enemy;
-    float timer;
-    [SerializeField] GameObject pistolEnemy;
-    [SerializeField] GameObject rifleEnemy;
-    [SerializeField] GameObject sniperEnemy;
-    [SerializeField] GameObject shotgunEnemy;
-    [SerializeField] float spawnInterval = 10f;
+    [SerializeField] GameObject enemy;
+    [SerializeField] float spawnInterval;
 
-    void Update() {
-        timer += Time.deltaTime;
-        if (timer >= spawnInterval) {
-            timer = 0f;
-            float randomEnemyIndex = Random.Range (0,2);
-            int randomSpawnIndex = Random.Range (0, transform.childCount - 1);
-            switch(randomEnemyIndex) {
-                case 0:
-                    enemy = pistolEnemy;
-                    break;
-                case 1:
-                    enemy = rifleEnemy;
-                    break;
-                case 2:
-                    enemy = sniperEnemy;
-                    break;
-                default:
-                    break;
-            }
-            Instantiate(enemy, transform.GetChild(randomSpawnIndex).position,  Quaternion.identity);
-            Debug.Log("Spawned an enemy!");
+
+    void Start() {
+        InvokeRepeating("SpawnRandomEnemy", spawnInterval, spawnInterval);
+    }
+
+    void SpawnRandomEnemy() {
+        int childCount = transform.childCount;
+        if (childCount == 0) {
+            Debug.LogWarning("No children to spawn enemy at!");
+            return;
         }
+        int randomIndex = Random.Range(0, childCount);
+        Transform randomChild = transform.GetChild(randomIndex);
+        Instantiate(enemy, randomChild.position, Quaternion.identity);
+        Debug.Log("Spawned enemy at index " + randomIndex);
     }
 }
